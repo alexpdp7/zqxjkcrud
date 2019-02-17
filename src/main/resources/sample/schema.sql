0d0c8ae309c80f5117e1bab591f905c234d34861
alex <alex@pdp7.net>
2019-02-17 11:28:28 +0100

    Use test user schema to prevent search_path issues

--- src/main/resources/sample/schema.sql
+++ src/main/resources/sample/schema.sql
@@ -8,17 +8,15 @@ create table sample_app.users (
 
 insert into sample_app.users(username, password) values ('admin', 'admin');
 
-create schema zqxjk;
+create schema test;
 
-create view zqxjk._users as
+create view test._users as
 	select username, password
 	from sample_app.users;
 
-create view zqxjk.users as
+create view test.users as
 	select id as _id,
 	       username as _friendly,
 	       username,
 	       password
 	from   sample_app.users;
-
-set search_path to zqxjk;
