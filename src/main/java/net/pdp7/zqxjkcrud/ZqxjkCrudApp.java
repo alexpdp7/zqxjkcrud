36dd88cc97ac4fc40d5964660d70395fd67e6675
alex <alex@pdp7.net>
2019-03-03 11:11:35 +0100

    Remove Guava dependency
    Remove map builders with Map.of[Entries]

--- src/main/java/net/pdp7/zqxjkcrud/ZqxjkCrudApp.java
+++ src/main/java/net/pdp7/zqxjkcrud/ZqxjkCrudApp.java
@@ -1,5 +1,9 @@
 package net.pdp7.zqxjkcrud;
 
+import static java.util.Map.entry;
+
+import java.util.Map;
+
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.google.common.collect.ImmutableMap;
 
 import net.pdp7.zqxjkcrud.dao.CatalogRepository;
 import net.pdp7.zqxjkcrud.dao.Dao;
@@ -55,28 +57,23 @@ public class ZqxjkCrudApp {
 	@RequestMapping("/")
 	public ModelAndView index() {
 		return new ModelAndView("index",
-				ImmutableMap.<String, Object>builder()
-				.put("tables", dao().getTables().values())
-				.build());
+				Map.of("tables", dao().getTables().values()));
 	}
 
 	@RequestMapping("/table/{name}")
 	public ModelAndView table(@PathVariable String name) {
 		Table table = dao().getTables().get(name);
 		return new ModelAndView("table",
-				ImmutableMap.<String, Object>builder()
-				.put("table", table)
-				.put("rows", table.getRows())
-				.build());
+				Map.ofEntries(
+						entry("table", table),
+						entry("rows", table.getRows())));
 	}
 
 	@RequestMapping("/table/{name}/new")
 	public ModelAndView newView(@PathVariable String name) {
 		Table table = dao().getTables().get(name);
 		return new ModelAndView("row",
-				ImmutableMap.<String, Object>builder()
-				.put("table", table)
-				.build());
+				Map.of("table", table));
 	}
 
 	public static void main(String[] args) {
