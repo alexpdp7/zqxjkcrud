e2dc26d187682953ac1d0d634b99995f8df55825
alex <alex@pdp7.net>
2019-03-16 19:43:50 +0100

    Refactor controller to appease static code checks

--- src/main/java/net/pdp7/zqxjkcrud/ZqxjkCrudApp.java
+++ src/main/java/net/pdp7/zqxjkcrud/ZqxjkCrudApp.java
@@ -1,35 +1,22 @@
 package net.pdp7.zqxjkcrud;
 
-import static java.util.Map.entry;
-
-import java.util.Map;
-
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.context.annotation.Configuration;
 
 import net.pdp7.zqxjkcrud.dao.CatalogRepository;
 import net.pdp7.zqxjkcrud.dao.Dao;
-import net.pdp7.zqxjkcrud.dao.Row;
-import net.pdp7.zqxjkcrud.dao.Table;
-import net.pdp7.zqxjkcrud.dao.Update;
 import net.pdp7.zqxjkcrud.security.UserDetailsServiceImpl;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
 
 @SpringBootApplication
-@Controller
+@Configuration
 public class ZqxjkCrudApp {
-
 	@Autowired
 	public DSLContext dslContext;
 
@@ -56,49 +43,6 @@ public class ZqxjkCrudApp {
 	@Bean
 	public Dao dao() {
 		return new Dao(catalogRepository(), dslContext);
-	}
-
-	@RequestMapping("/")
-	public ModelAndView index() {
-		return new ModelAndView("index",
-				Map.of("tables", dao().getTables().values()));
-	}
-
-	@RequestMapping("/table/{name}")
-	public ModelAndView table(@PathVariable String name) {
-		Table table = dao().getTables().get(name);
-		return new ModelAndView("table",
-				Map.ofEntries(
-						entry("table", table),
-						entry("rows", table.getRows())));
-	}
-
-	@RequestMapping("/table/{name}/new")
-	public ModelAndView newView(@PathVariable String name) {
-		Table table = dao().getTables().get(name);
-		return new ModelAndView("row",
-				Map.ofEntries(
-						entry("table", table),
-						entry("row", new Row.TransientRow()),
-						entry("action", Update.TableAction.INSERT)));
-	}
-
-	@RequestMapping("/table/{name}/row/{id}")
-	public ModelAndView rowView(@PathVariable String name, @PathVariable String id) {
-		Table table = dao().getTables().get(name);
-		Row row = table.getRow(id);
-		return new ModelAndView("row",
-				Map.ofEntries(
-						entry("table", table),
-						entry("row", row),
-						entry("action", Update.TableAction.UPDATE)));
-	}
-
-	@PostMapping("/update")
-	public String update(String next, WebRequest request) {
-		Update update = new Update(request.getParameterMap());
-		dao().update(update);
-		return "redirect:" + next;
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/net/pdp7/zqxjkcrud/mvc/ZqxjkCrudController.java
@@ -0,0 +1,68 @@
+package net.pdp7.zqxjkcrud.mvc;
+
+import static java.util.Map.entry;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import net.pdp7.zqxjkcrud.dao.Dao;
+import net.pdp7.zqxjkcrud.dao.Row;
+import net.pdp7.zqxjkcrud.dao.Table;
+import net.pdp7.zqxjkcrud.dao.Update;
+
+@Controller
+public class ZqxjkCrudController {
+
+	@Autowired
+	protected Dao dao;
+
+	@RequestMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index",
+				Map.of("tables", dao.getTables().values()));
+	}
+
+	@RequestMapping("/table/{name}")
+	public ModelAndView table(@PathVariable String name) {
+		Table table = dao.getTables().get(name);
+		return new ModelAndView("table",
+				Map.ofEntries(
+						entry("table", table),
+						entry("rows", table.getRows())));
+	}
+
+	@RequestMapping("/table/{name}/new")
+	public ModelAndView newView(@PathVariable String name) {
+		Table table = dao.getTables().get(name);
+		return new ModelAndView("row",
+				Map.ofEntries(
+						entry("table", table),
+						entry("row", new Row.TransientRow()),
+						entry("action", Update.TableAction.INSERT)));
+	}
+
+	@RequestMapping("/table/{name}/row/{id}")
+	public ModelAndView rowView(@PathVariable String name, @PathVariable String id) {
+		Table table = dao.getTables().get(name);
+		Row row = table.getRow(id);
+		return new ModelAndView("row",
+				Map.ofEntries(
+						entry("table", table),
+						entry("row", row),
+						entry("action", Update.TableAction.UPDATE)));
+	}
+
+	@PostMapping("/update")
+	public String update(String next, WebRequest request) {
+		Update update = new Update(request.getParameterMap());
+		dao.update(update);
+		return "redirect:" + next;
+	}
+}
