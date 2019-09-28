e5c00ffa6ff9aeaea24f9e28256f758073d4efd1
alex <alex@pdp7.net>
2019-09-28 20:31:07 +0200

    Disable CSRF only for SAML interaction routes

--- src/main/java/net/pdp7/zqxjkcrud/security/SamlConfiguration.java
+++ src/main/java/net/pdp7/zqxjkcrud/security/SamlConfiguration.java
@@ -20,7 +20,7 @@ public class SamlConfiguration extends WebSecurityConfigurerAdapter {
 			@Override
 			public void configure(HttpSecurity http) {
 				try {
-					http.csrf();
+					http.antMatcher("/saml/SSO").csrf().disable();
 					http.anonymous().disable();
 				} catch (Exception e) {
 					throw new RuntimeException(e);
