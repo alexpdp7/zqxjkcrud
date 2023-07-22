95c848a7d825e24545522a335fb37547fb50939d
alex <alex@pdp7.net>
2023-07-22 10:08:18 +0000

    Use Docker instead of Podman
    (You can still use Podman with docker-podman, but hopefully this solves
    issues with GitHub Actions.

--- build.py
+++ build.py
@@ -27,7 +27,7 @@ def build_images_and_push():
     for tag in tags:
         tag = re.sub(r"\W", "_", tag)
         quay_expires_after = "never" if not is_dirty() and tag == "master" else "3d"
-        _run(["podman", "build", ".", "--build-arg", f"QUAY_EXPIRES_AFTER={quay_expires_after}", "-t", f"zqxjkcrud:{tag}"])
+        _run(["docker", "build", ".", "--build-arg", f"QUAY_EXPIRES_AFTER={quay_expires_after}", "-t", f"quay.io/alexpdp7/zqxjkcrud:{tag}"])
         assert tag != "dirty", "attempting to push dirty image"
-        _run(["podman", "push", f"zqxjkcrud:{tag}", f"quay.io/alexpdp7/zqxjkcrud:{tag}"])
+        _run(["docker", "push", f"quay.io/alexpdp7/zqxjkcrud:{tag}"])
     return tags
